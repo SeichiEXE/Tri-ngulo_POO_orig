b61a9b6cf8d057243b3a7cd0922e911e5a379247
SeichiEXE <127798960+SeichiEXE@users.noreply.github.com>
2023-05-15 23:50:42 -0300

    Update

--- Calculadora_Triangulos_Classes.cpp
+++ Calculadora_Triangulos_Classes.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 using namespace std;
 
-//inserindo fórmulas de calculo e variaveis para a formula
+// Função para calcular a área de um triângulo
 double calcularAreaTriangulo(double a, double b, double c) {
     double p = (a + b + c) / 2;
     return sqrt(p * (p - a) * (p - b) * (p - c));
@@ -11,23 +11,23 @@ double calcularAreaTriangulo(double a, double b, double c) {
 int main() {
     double xa, xb, xc, ya, yb, yc;
     
-    //interface de usuario 1
+    // Interface do usuário 1: Solicita as medidas dos lados do triângulo X
     cout << "Digite as medidas dos lados do triângulo X (separadas por espaço): ";
     cin >> xa >> xb >> xc;
     
-    //interface de usuario 2
+    // Interface do usuário 2: Solicita as medidas dos lados do triângulo Y
     cout << "Digite as medidas dos lados do triângulo Y (separadas por espaço): ";
     cin >> ya >> yb >> yc;
     
-    //código calculando as variaveis inseridas pelo usuario 
+    // Calcula a área dos triângulos X e Y
     double areaX = calcularAreaTriangulo(xa, xb, xc);
     double areaY = calcularAreaTriangulo(ya, yb, yc);
     
-    //Resultado do calculo das áreas
+    // Exibe as áreas calculadas
     cout << "Área do triângulo X: " << areaX << endl;
     cout << "Área do triângulo Y: " << areaY << endl;
     
-    //condição de fator de resultado (triangulo maior ou menor)
+    // Verifica qual triângulo tem a maior área
     if (areaX > areaY) {
         cout << "O triângulo X tem a maior área." << endl;
     }
@@ -35,7 +35,7 @@ int main() {
         cout << "O triângulo Y tem a maior área." << endl;
     }
     else {
-        cout << "Os triângulos possuem a mesma área" << endl;
+        cout << "Os triângulos possuem a mesma área." << endl;
     }
     return 0;
 }
